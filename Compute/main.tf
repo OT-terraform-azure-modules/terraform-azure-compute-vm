797190c4b7e547f395417055b5e351e39861a530
Naveen Verma <61639221+naveenverma023@users.noreply.github.com>
2025-03-11 19:22:22 +0530

    Update vm sku in main.tf

--- Compute/main.tf
+++ Compute/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_linux_virtual_machine" "modular_vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "22_04-lts"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 }
